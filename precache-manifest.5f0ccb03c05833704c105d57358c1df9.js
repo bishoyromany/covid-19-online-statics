cdf5d6277ba1d4c3fa604107c0fa804834a010df
Bishoy Romany <7bishoyromany7@gmail.com>
2020-04-09 18:15:00 +0200

    SERVE

---
+++ precache-manifest.5f0ccb03c05833704c105d57358c1df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb586b54863fbd30ab963b1a0dbf4787",
+    "url": "/covid-19-online-statics/index.html"
+  },
+  {
+    "revision": "5ee56dd049a1c54ae650",
+    "url": "/covid-19-online-statics/static/css/main.d4740025.chunk.css"
+  },
+  {
+    "revision": "2eb09c1014ad817263ab",
+    "url": "/covid-19-online-statics/static/js/2.196559b0.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/covid-19-online-statics/static/js/2.196559b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee56dd049a1c54ae650",
+    "url": "/covid-19-online-statics/static/js/main.698f6145.chunk.js"
+  },
+  {
+    "revision": "69a041f5ad40252ed390",
+    "url": "/covid-19-online-statics/static/js/runtime-main.0a838fca.js"
+  },
+  {
+    "revision": "2b4471cd489804736605cc69a88c13a7",
+    "url": "/covid-19-online-statics/static/media/header_background.2b4471cd.jpg"
+  }
+]);
